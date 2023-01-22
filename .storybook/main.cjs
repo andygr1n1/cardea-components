0726d0f2d9c015d28b0bb0749db0e2d8ff37428d
Grini Andrei <Andrei.Grini@carasent.com>
2023-01-22 19:41:41 +0200

    cardea base setup

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,11 +1,6 @@
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        'storybook-addon-toolbar-actions/register',
-    ],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: '@storybook/react',
     core: {
         builder: '@storybook/builder-vite',
